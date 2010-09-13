fe14afc3fea0ad9106e0ad5a24ae760106e94413
James Harton <jamesotron@gmail.com>
2010-09-14 11:33:01 +1200

    Crappy old-style plugin init

---
+++ init.rb
@@ -0,0 +1,3 @@
+require File.join(File.dirname(__FILE__), 'lib', 'mongoid', 'find_by')
+require File.join(File.dirname(__FILE__), 'lib', 'mongoid', 'find_or_create_by')
+require File.join(File.dirname(__FILE__), 'lib', 'mongoid', 'dynamic_finders')
